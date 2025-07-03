252eba083a03aacdc9480268b1a273db6c9370d8
Hampus Avekvist <hampus.avekvist@hey.com>
2025-07-03 17:07:12 +0200

    chore: Set up Nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "GameBoy++";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+            xmake
+            ninja
+            gcc15
+            stdenv.cc.cc
+            mold
+            gdb
+            llvm
+            llvmPackages_20.clang-tools
+            python314
+            pkg-config
+            libGL
+            xorg.libX11
+        ];
+    };
+  };
+}
